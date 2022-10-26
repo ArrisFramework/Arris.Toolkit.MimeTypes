1ce2cdba8a28a8693e7220a5f1f872db90c4548d
Karel Wintersky <karel.wintersky@yandex.ru>
2022-10-26 09:43:49 +0300

    0.4
    added template, generate script and customize.json

---
+++ README.md
@@ -0,0 +1,82 @@
+# Mimetypes
+
+Creates a JSON document containing a thorough list of file extensions => mime types as provided by the
+[Apache httpd project](http://httpd.apache.org).
+
+## How to update?
+
+### Step 1
+
+Download the latest copy of the Apache `mime-types` file into the same directory as the `generate` script
+
+```bash
+make update
+```
+
+or 
+```bash
+wget --no-check-certificate https://svn.apache.org/repos/asf/httpd/httpd/branches/2.4.x/docs/conf/mime.types -O ./tools/mime.types
+```
+
+### Step 2 (optional)
+
+Update the provided `customize.json` document with any additional mimetypes to define, or any that you would like to
+override.
+
+### Step 3
+
+Run the `generate` script:
+
+```bash
+make build
+```
+
+In the end, a `mimetypes.json` document will be generated. This JSON document can be easily parsed into a
+map/dictionary/associative array by pretty much every programming language with little effort.
+
+It also generates a backing PHP class if you want to use the data in PHP-land.
+
+### Step 4 
+
+Update repository (commit, push, pull request)
+
+## Installation
+
+### Install with Composer
+
+If you're using [Composer](http://getcomposer.org) to manage dependencies, you can add the mimetypes with it.
+
+```json
+{
+    "require": {
+        "karelwintersky/arris.toolkit.mimetypes": ">=1.0"
+    }
+}
+```
+or
+```
+composer require karelwintersky/arris.toolkit.mimetypes
+```
+
+## Usage
+
+And use it in your scripts:
+
+```php
+
+use Arris\Toolkit;
+$type = Mimetypes::getInstance()->fromExtension('html');
+
+// => text/html
+```
+
+## License & Copyright
+
+Original idea: 2010-2013 [Ryan Parman](http://ryanparman.com).
+
+Refactored: 2022 [Karel Wintersky](https://github.com/KarelWintersky)
+
+removed unnecessary Twig template engine and `dflydev/apache-mime-types` (repository load/mapper)
+
+Licensed for use under the terms of the [MIT license](http://www.opensource.org/licenses/mit-license.php).
+
---
+++ tools/generate.php
@@ -0,0 +1,109 @@
+#! /usr/bin/env php
+<?php
+
+function var_export_short($expression, $return = false): ?string
+{
+    if (!is_array($expression)) {
+        return var_export($expression, $return);
+    }
+    $export = var_export($expression, true);
+    $export = preg_replace("/^([ ]*)(.*)/m", '$1$1$2', $export);
+    $array = preg_split("/\r\n|\n|\r/", $export);
+    $array = preg_replace(["/\s*array\s\($/", "/\)(,)?$/", "/\s=>\s$/"], [NULL, ']$1', ' => ['], $array);
+    $export = implode(PHP_EOL, array_filter(["["] + $array));
+    if ($return) {
+        return $export;
+    } else {
+        echo $export;
+        return '';
+    }
+}
+
+$template_generate_date = date('j M Y, g:ia T');
+
+$downloaded_mimes = file(__DIR__ . '/mime.types', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+if (empty($downloaded_mimes)) {
+    die('mime.types is empty or not downloded');
+}
+
+$lines = [];
+
+foreach ($downloaded_mimes as $line) {
+    if (strpos($line, '#') !== 0) {
+        preg_match_all('/^((\w|\/|\.|-|\+)+)(\s+)([^\n]*)$/im', $line, $match);
+        $type = $match[1][0];
+        $extensions = explode(' ', $match[4][0]);
+
+        $lines[$type] = $extensions;
+    }
+}
+
+$mimes = [];
+
+foreach ($lines as $type => $extensions) {
+    foreach ($extensions as $extension) {
+        if (!isset($lines[$extension])) {
+            $mimes[$extension] = $type;
+        }
+    }
+}
+$customize_json = __DIR__ . '/customize.json';
+
+if (file_exists($customize_json)) {
+    $entries = json_decode(file_get_contents($customize_json), true);
+
+    foreach ($entries as $extensions => $type) {
+        $extensions = explode(' ', $extensions);
+
+        foreach ($extensions as $extension) {
+            $mimes[$extension] = $type;
+        }
+    }
+}
+
+// WRITE JSON DOCUMENT
+ksort($mimes);
+
+$mimetypes_json = __DIR__ . '/mimetypes.json';
+
+if (file_put_contents($mimetypes_json, json_encode($mimes, JSON_PRETTY_PRINT))) {
+    echo print_r($mimes, true)
+        . PHP_EOL
+        . "\033[01;32mSuccessfully wrote {$mimetypes_json}\033[00m"
+        . PHP_EOL;
+} else {
+    echo "Failed to write {$mimetypes_json}. Please ensure that this file system location is writable." . PHP_EOL;
+}
+
+/*$max_ext_length = 0;
+foreach ($mimes as $ext => $type) {
+    $max_ext_length = max($max_ext_length, strlen($ext));
+}
+
+$text = '';
+$text .= "return [" . PHP_EOL;
+foreach ($mimes as $ext => $type) {
+    $text .= "            '{$ext}' ";
+    // $text .= str_repeat(' ', $max_ext_length + 2 - strlen($ext));
+    $text .= "=> '{$type}'," . PHP_EOL;
+}
+$text .= "        ];";*/
+
+# WRITE PHP CLASS
+$content = file_get_contents(__DIR__ . '/template.txt');
+$content = str_replace('%%generate_datetime%%', $template_generate_date, $content);
+$content = str_replace('%%array_mime_types%%', "return " . var_export_short($mimes, true) . ";", $content);
+
+//$content = str_replace('%%array_mime_types%%', $text, $content); // преформатированный красивый вывод
+
+file_put_contents(
+    __DIR__ . '/../src/Mimetypes.php',
+    $content
+);
+
+echo "\033[01;32mSuccessfully wrote " . __DIR__ . '/src/Mimetypes.php' . "\033[00m" . PHP_EOL;
+
+// Done.
+echo PHP_EOL;
+
